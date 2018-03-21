40d2219c51e87d0871e32da8d6cb771a6f7bdf1e
amelieoller <amelieoller@gmail.com>
2018-03-21 13:27:04 -0700

    Add NoteCard component that renders notes with title and body

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
 import styles from './App.css';
+import NoteCard from '../NoteCard/NoteCard';
+import _ from 'lodash';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			title: '',
+			body: '',
+			notes: [
+				{
+					title: 'First Note Title',
+					body: 'First Note Body'
+				},
+				{
+					title: 'Second Note Title',
+					body: 'Second Note Body'
+				}
+			]
+		};
+	}
+
+	renderNotes() {
+		return _.map(this.state.notes, (note, key) => {
+			return (
+				<NoteCard key={key}>
+					<h3>{note.title}</h3>
+					<p>{note.body}</p>
+				</NoteCard>
+			);
+		});
+	}
+
 	render() {
 		return (
 			<div className={styles.container}>
 				<h1>Note Vote</h1>
+				{this.renderNotes()}
 			</div>
 		);
 	}
---
+++ src/components/NoteCard/NoteCard.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './NoteCard.css';
+
+const NoteCard = ({ children }) => <div>{children}</div>;
+
+export default NoteCard;
