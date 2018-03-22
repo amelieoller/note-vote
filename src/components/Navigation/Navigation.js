5616095d2bc6083a9984ff40f62fdafe6aceba73
amelieoller <amelieoller@gmail.com>
2018-03-22 16:32:37 -0700

    Add logout to navigation when user is logged in

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Navigation.scss';
+import { logout } from '../../actions/userActions';
+import { connect } from 'react-redux';
 
-const Navigation = () => (
-	<div className={styles.nav}>
-		<ul>
-			<li className={styles.home}>
-				<Link to="/">Home</Link>
-			</li>
-			<li className={styles.contact}>
-				<Link to="/login" className={styles.active}>
-					Login
-				</Link>
-			</li>
-		</ul>
-	</div>
-);
+class Navigation extends Component {
+	render() {
+		return (
+			<div className={styles.nav}>
+				<ul>
+					<li className={styles.home}>
+						<Link to="/">Home</Link>
+					</li>
 
-export default Navigation;
+					<li className={styles.contact}>
+						{this.props.user === null ? (
+							<Link to="/login" className={styles.active}>
+								Login
+							</Link>
+						) : (
+							<Link
+								to="/login"
+								className={styles.active}
+								onClick={() => this.props.logout()}
+							>
+								Logout
+							</Link>
+						)}
+					</li>
+				</ul>
+			</div>
+		);
+	}
+}
+
+const mapStateToProps = state => {
+	return {
+		user: state.user
+	};
+};
+
+export default connect(mapStateToProps, { logout })(Navigation);
