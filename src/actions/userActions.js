5952c2ef91b803421148e482a9d8e91675d17943
amelieoller <amelieoller@gmail.com>
2018-03-23 11:45:17 -0700

    Add user status to actions

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,5 +1,5 @@
 import { auth, googleProvider } from '../firebase';
-import { GET_USER } from './actionTypes';
+import { GET_USER, USER_STATUS } from './actionTypes';
 
 export function googleLogin() {
 	return dispatch => auth.signInWithPopup(googleProvider);
@@ -14,13 +14,26 @@ export function passwordSignIn(email, password) {
 }
 
 export function getUser() {
-	return dispatch =>
+	return dispatch => {
+		// As soon as function fires set loading to true
+		dispatch({
+			type: USER_STATUS,
+			payload: true
+		});
+
 		auth.onAuthStateChanged(user => {
 			dispatch({
 				type: GET_USER,
 				user: user
 			});
+
+			// Set loading status to false
+			dispatch({
+				type: USER_STATUS,
+				payload: false
+			});
 		});
+	};
 }
 
 export function logout() {
