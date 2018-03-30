6b1de92ff0a655ed376ea384d736a65604e77e0b
amelieoller <amelieoller@gmail.com>
2018-03-30 15:52:41 -0700

    Add styles to note form

--- src/components/NoteForm/NoteForm.js
+++ src/components/NoteForm/NoteForm.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import styles from './NoteForm.scss'
 
 const NoteForm = ({handleChange, handleSubmit, state}) => {
 	return (
-		<div>
+		<div className={styles.container}>
+		<h2>Create a New Note:</h2>
 			<form onSubmit={handleSubmit}>
 				<input type="text" name="title" value={state.title} placeholder="Title" onChange={handleChange} />
 				<br/>
