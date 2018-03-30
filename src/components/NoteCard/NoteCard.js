867ca5861b864935460f9f93b039dac99262017d
amelieoller <amelieoller@gmail.com>
2018-03-30 15:41:38 -0700

    Edit text and button alignment

--- src/components/NoteCard/NoteCard.js
+++ src/components/NoteCard/NoteCard.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import styles from './NoteCard.scss';
-import Vote from '../Vote/Vote'
+import Vote from '../Vote/Vote';
 
 const NoteCard = ({ id, note, deleteNote }) => {
 	return (
 		<div className={styles.container}>
-			<h3>{note.title}</h3>
-			<p>{note.body}</p>
-         <button onClick={() => deleteNote(id)}>Delete</button>
-         <Vote note={note} id={id} />
+			<div className={styles.text}>
+				<h3>{note.title}</h3>
+				<p>{note.body}</p>
+			</div>
+			<div className={styles.buttons}>
+				<div className={styles.delete}>
+					<button onClick={() => deleteNote(id)}>Delete</button>
+				</div>
+				<div className={styles.vote}>
+					<Vote note={note} id={id} />
+				</div>
+			</div>
 		</div>
 	);
 };
