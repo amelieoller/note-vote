0303efb9385c451908a4a5fa82c054096dbf0dfd
amelieoller <amelieoller@gmail.com>
2018-03-30 15:36:30 -0700

    Update note card container css

--- src/components/NoteCard/NoteCard.js
+++ src/components/NoteCard/NoteCard.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import styles from './NoteCard.css';
+import styles from './NoteCard.scss';
 import Vote from '../Vote/Vote'
 
 const NoteCard = ({ id, note, deleteNote }) => {
 	return (
-		<div>
+		<div className={styles.container}>
 			<h3>{note.title}</h3>
 			<p>{note.body}</p>
          <button onClick={() => deleteNote(id)}>Delete</button>
