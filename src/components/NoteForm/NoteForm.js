7a820173badd0d172d68775759b48bc1b62032e0
amelieoller <amelieoller@gmail.com>
2018-04-04 08:06:39 -0700

    Update form css

--- src/components/NoteForm/NoteForm.js
+++ src/components/NoteForm/NoteForm.js
@@ -6,9 +6,9 @@ const NoteForm = ({handleChange, handleSubmit, state}) => {
 		<div className={styles.container}>
 		<h2>Create a New Note:</h2>
 			<form onSubmit={handleSubmit}>
-				<input type="text" name="title" value={state.title} placeholder="Title" onChange={handleChange} />
+				<input className={styles.fullWidth} type="text" name="title" value={state.title} placeholder="Title" onChange={handleChange} />
 				<br/>
-				<textarea type="text" name="body" value={state.body} placeholder="Body" onChange={handleChange} />
+				<textarea className={styles.fullWidth} type="text" name="body" value={state.body} placeholder="Body" onChange={handleChange} />
 				<br/>
 				<input type="submit"/>
 			</form>
