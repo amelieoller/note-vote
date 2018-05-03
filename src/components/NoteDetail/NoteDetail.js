42e86a9ae782378e91ff5d6e62dbac6acb12dd1a
amelieoller <amelieoller@gmail.com>
2018-05-03 08:25:43 -0700

    Add note detail update functionality

--- src/components/NoteDetail/NoteDetail.js
+++ src/components/NoteDetail/NoteDetail.js
@@ -1,8 +1,37 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { updateNote } from '../../actions/noteActions';
 
 class NoteDetail extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			title: this.props.note.title || '',
+			body: this.props.note.body || ''
+		};
+
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleChange = e => {
+		this.setState({
+			[e.target.name]: e.target.value
+		});
+	};
+
+	handleClick = e => {
+		e.preventDefault();
+		if (this.state.title && this.state.body) {
+			this.props.updateNote(this.props.noteId, {
+				title: this.state.title,
+				body: this.state.body
+			});
+			this.props.history.push('/');
+		}
+	};
+
 	render() {
 		const { note } = this.props;
 		return (
@@ -10,10 +39,23 @@ class NoteDetail extends Component {
 				<Link to="/">Back</Link>
 				{/* Fix this to get state after refresh */}
 				{note && (
-					<div>
-						<h1>{note.title}</h1>
-						<p>{note.body}</p>
-					</div>
+					<form>
+						<input
+							onChange={this.handleChange}
+							type="text"
+							value={this.state.title}
+							name="title"
+						/>
+						<textarea
+							onChange={this.handleChange}
+							type="text"
+							value={this.state.body}
+							name="body"
+						/>
+						<button onClick={this.handleClick} type="submit">
+							Change Note
+						</button>
+					</form>
 				)}
 			</div>
 		);
@@ -23,8 +65,9 @@ class NoteDetail extends Component {
 function mapStateToProps(state, ownProps) {
 	return {
 		note: state.notes[ownProps.match.params.id],
+		noteId: ownProps.match.params.id,
 		uid: state.user.uid
 	};
 }
 
-export default connect(mapStateToProps)(NoteDetail);
+export default connect(mapStateToProps, { updateNote })(NoteDetail);
