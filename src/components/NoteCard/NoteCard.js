cb0259bd0f7fb6798a756bd71d4e1c63800884e4
amelieoller <amelieoller@gmail.com>
2018-05-06 11:38:16 -0700

    Display categories on note card

--- src/components/NoteCard/NoteCard.js
+++ src/components/NoteCard/NoteCard.js
@@ -3,7 +3,7 @@ import styles from './NoteCard.scss';
 import Vote from '../Vote/Vote';
 import { Link } from 'react-router-dom';
 
-const NoteCard = ({ id, note, deleteNote, user }) => {
+const NoteCard = ({ id, note, deleteNote, user, categories }) => {
 	return (
 		<div className={styles.container}>
 			<div className={styles.text}>
@@ -11,6 +11,16 @@ const NoteCard = ({ id, note, deleteNote, user }) => {
 					<h3>{note.title}</h3>
 				</Link>
 				<p>{note.body}</p>
+				<h3>Categories:</h3>
+				{note.categories &&
+					note.categories.map(cat => {
+						for (var key in categories) {
+							if (key === cat) {
+								var newKey = id+key
+								return <li key={newKey}>{categories[key].name}</li>;
+							}
+						}
+					})}
 			</div>
 			<div className={styles.buttons}>
 				{note.uid === user.uid && (
