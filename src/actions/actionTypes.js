60262da3dcce968d1aeaccf75251240e4adb5dbb
amelieoller <amelieoller@gmail.com>
2018-05-16 21:19:39 -0700

    Add/sort action types

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,9 +1,21 @@
+// Notes
 export const GET_NOTES = 'GET_NOTES';
-export const GET_USER = 'GET_USER';
-export const GET_CATEGORIES = 'GET_CATEGORIES';
-
 export const NOTES_STATUS = 'NOTES_STATUS';
+export const ADD_NOTE = 'ADD_NOTE';
+export const DELETE_NOTE = 'DELETE_NOTE';
+
+// Users
+export const GET_USER = 'GET_USER';
 export const USER_STATUS = 'USER_STATUS';
+
+// Categories
+export const GET_CATEGORIES = 'GET_CATEGORIES';
 export const CATEGORIES_STATUS = 'CATEGORIES_STATUS';
 
+// Tags
+export const GET_TAGS = 'GET_TAGS';
+export const TAGS_STATUS = 'TAG_STATUS';
+
+// Others
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
 export const ADD_ERROR = 'ADD_ERROR';
