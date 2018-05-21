d7f4475e4974dd94a7cc83fe8d6d1b7a6ec14160
amelieoller <amelieoller@gmail.com>
2018-05-20 17:01:22 -0700

    Add update note case to notes reducer

--- src/reducers/notesReducer.js
+++ src/reducers/notesReducer.js
@@ -1,9 +1,14 @@
-import { GET_NOTES } from '../actions/actionTypes';
+import { GET_NOTES, UPDATE_NOTE } from '../actions/actionTypes';
 
 export default function(state = {}, action) {
 	switch (action.type) {
 		case GET_NOTES:
 			return action.notes;
+		case UPDATE_NOTE:
+			let id = action.payload.id;
+			let votes = action.payload.note.votes;
+			var newNote = { ...state[id], votes: votes };
+			return { ...state, [id]: newNote };
 		default:
 			return state;
 	}
