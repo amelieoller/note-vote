d002194d0c3c88d9e7a1cfb6e057cce0e07fde79
amelieoller <amelieoller@gmail.com>
2018-05-20 17:06:15 -0700

    Update vote component to include 'vote lock' functionality

--- src/components/Vote/Vote.js
+++ src/components/Vote/Vote.js
@@ -1,46 +1,61 @@
 import React, { Component } from 'react';
 import styles from './Vote.scss';
 import { connect } from 'react-redux';
-import { updateNote } from '../../actions/noteActions';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-import faCopy from '@fortawesome/fontawesome-free-solid/faCopy';
 import faThumbsUp from '@fortawesome/fontawesome-free-solid/faThumbsUp';
 import faThumbsDown from '@fortawesome/fontawesome-free-solid/faThumbsDown';
+
+// Actions
+import { updateNote } from '../../actions/noteActions';
 
 class Vote extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			votes: this.props.note.votes
+			voteLock: false
 		};
 	}
 
 	handleClick(e) {
-		let newVote = this.state.votes;
+		let newVote = this.props.note.votes;
 		e.currentTarget.name === 'plus' ? newVote++ : newVote--;
 
 		this.props.updateNote(this.props.id, { votes: newVote });
 
 		this.setState({
-			votes: newVote
+			voteLock: true
 		});
+
+		setTimeout(() => {
+			this.setState({ voteLock: false });
+		}, 2000);
 	}
 
 	render() {
 		return (
-			<div className={styles.center}>
-				<button title="UpVote" name="plus" onClick={e => this.handleClick(e)}>
-					<FontAwesomeIcon icon={faThumbsUp} />
-				</button>
-				<span className={styles.vote}>{this.state.votes}</span>
-				<button
-					title="DownVote"
-					name="minus"
-					onClick={e => this.handleClick(e)}
-				>
-					<FontAwesomeIcon icon={faThumbsDown} />
-				</button>
+			<div className={styles.container}>
+				<div>
+					<button
+						disabled={this.state.voteLock}
+						title="UpVote"
+						name="plus"
+						onClick={e => this.handleClick(e)}
+					>
+						<FontAwesomeIcon icon={faThumbsUp} />
+					</button>
+				</div>
+				<span className={styles.vote}>{this.props.note.votes}</span>
+				<div>
+					<button
+						disabled={this.state.voteLock}
+						title="DownVote"
+						name="minus"
+						onClick={e => this.handleClick(e)}
+					>
+						<FontAwesomeIcon icon={faThumbsDown} />
+					</button>
+				</div>
 			</div>
 		);
 	}
