dcb52b8afedc8da9e01ae3aaa1de663d2f102d98
amelieoller <amelieoller@gmail.com>
2018-05-20 17:02:30 -0700

    Add tags sand visibility filter to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,13 +4,17 @@ import user from './userReducer';
 import loading from './loadingReducer';
 import errors from './errorsReducer';
 import categories from './categoriesReducer';
+import tags from './tagsReducer';
+import visibilityFilter from './visibilityFilter'
 
 const rootReducer = combineReducers({
 	notes,
 	user,
 	loading,
 	errors,
-	categories
+	categories,
+	tags,
+	visibilityFilter
 });
 
 export default rootReducer;
