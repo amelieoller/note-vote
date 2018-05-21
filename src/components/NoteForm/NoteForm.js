39ed92b25157f1177b2e76488f7a2d8c93bf7c73
amelieoller <amelieoller@gmail.com>
2018-05-20 17:45:41 -0700

    Update note form to include categories and tags

--- src/components/NoteForm/NoteForm.js
+++ src/components/NoteForm/NoteForm.js
@@ -1,66 +1,53 @@
 import React from 'react';
 import styles from './NoteForm.scss';
-import _ from 'lodash';
+import Checkboxes from '../Checkboxes/Checkboxes';
 
-const NoteForm = ({ handleChange, handleSubmit, state, categories }) => {
-	return (
-		<div className={styles.formContainer}>
-			<form onSubmit={handleSubmit}>
-				<input
-					className={styles.fullWidth}
-					type="text"
-					name="title"
-					value={state.title}
-					placeholder="Title"
-					onChange={handleChange}
-				/>
-				<br />
-				<textarea
-					className={styles.fullWidth}
-					type="text"
-					name="body"
-					value={state.body}
-					placeholder="Body"
-					onChange={handleChange}
-				/>
-				<br />
-				<input
-					className={styles.fullWidth}
-					type="text"
-					name="category"
-					value={state.category}
-					placeholder="Category"
-					onChange={handleChange}
-				/>
-				<div className={styles.categoryContainer}>
-					{_.map(categories, (category, id) => {
-						return (
-							<div key={category[0]} className={styles.coloured}>
-								<div className={styles.checkbox}>
-									<label>
-										<input
-											type="checkbox"
-											name="categories"
-											id={category[0]}
-											onChange={handleChange}
-											checked={state.categories.includes(category[0])}
-										/>
-										<span className={styles.checkboxMaterial}>
-											<span className={styles.check} />
-										</span>{' '}
-										{category[1].name}
-									</label>
-								</div>
-							</div>
-						);
-					})}
-				</div>
-				<br />
-
-				<input type="submit" />
-			</form>
+const NoteForm = ({
+	categories,
+	tags,
+	handleNoteSubmit,
+	handleNoteChange,
+	state
+}) => (
+	<form onSubmit={handleNoteSubmit} className={styles.formContainer}>
+		<input
+			className={styles.fullWidth}
+			type="text"
+			name="title"
+			value={state.title}
+			placeholder="Title"
+			onChange={handleNoteChange}
+		/>
+		<textarea
+			className={styles.fullWidth}
+			type="text"
+			name="body"
+			value={state.body}
+			placeholder="Body"
+			onChange={handleNoteChange}
+		/>
+		<div className={styles.categories}>
+			<h3>Categories:</h3>
+			<Checkboxes
+				type="categories"
+				collection={categories}
+				handleNoteChange={handleNoteChange}
+				state={state}
+			/>
 		</div>
-	);
-};
+		<div className={styles.submit}>
+			<input type="submit" />
+		</div>
+		<div className={styles.tags}>
+			<h3>Tags:</h3>
+			<Checkboxes
+				type="tags"
+				collection={tags}
+				handleNoteChange={handleNoteChange}
+				state={state}
+			/>
+		</div>
+	</form>
+);
 
 export default NoteForm;
