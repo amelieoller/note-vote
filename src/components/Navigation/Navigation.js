2c1930b6e230c79dd3a1acf4ce2fd30f30fcc610
amelieoller <amelieoller@gmail.com>
2018-05-20 17:06:45 -0700

    Add 'add note' button to navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,6 +3,7 @@ import styles from './Navigation.scss';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import SearchInput from 'react-search-input';
 
 // Actions
 import { logout } from '../../actions/userActions';
@@ -10,35 +11,50 @@ import { logout } from '../../actions/userActions';
 class Navigation extends Component {
 	render() {
 		return (
-				<nav className={styles.navigation}>
-					<ul className={styles.categories}>
-						{this.props.user === null ? (
-							<li className={styles.categoryItem}>
-								<NavLink exact to="/login" activeClassName={styles.active}>
-									Login
-								</NavLink>
-							</li>
-						) : (
-							<span>
-								<li className={styles.categoryItem}>
-									<NavLink exact to="/" activeClassName={styles.active}>
-										Home
-									</NavLink>
-								</li>
-								<li className={styles.categoryItem}>
-									<NavLink
-										exact
-										to="/login"
-										activeClassName={styles.active}
-										onClick={() => this.props.logout()}
-									>
-										Logout
-									</NavLink>
-								</li>
-							</span>
-						)}
+			<nav className={styles.navigation}>
+				{this.props.user !== null && (
+					<ul>
+						<li>
+							<NavLink exact to="/" activeClassName={styles.active}>
+								Home
+							</NavLink>
+						</li>
+						<li>
+							<NavLink
+								exact
+								to="/login"
+								activeClassName={styles.active}
+								onClick={() => this.props.logout()}
+							>
+								Logout
+							</NavLink>
+						</li>
+						<li>
+							<a
+								className={styles.hideForm}
+								onClick={this.props.handleHideForm}
+							>
+								{this.props.formHidden ? (
+									<div>
+										<span className={styles.addNoteButton}>+</span>
+										<span className={styles.addNoteText}>Add Note</span>
+									</div>
+								) : (
+									<div>
+										<span className={styles.addNoteButton}>-</span>
+										<span className={styles.addNoteText}>Hide Form</span>
+									</div>
+								)}
+							</a>
+						</li>
 					</ul>
-				</nav>
+				)}
+
+				<SearchInput
+					className={styles.searchInput}
+					onChange={this.props.searchUpdated}
+				/>
+			</nav>
 		);
 	}
 }
